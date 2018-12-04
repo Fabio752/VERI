203ecfcbae53c06e4a3cf8afe9ca4909eaefaacf
Fabio Deo <36317183+Fabio752@users.noreply.github.com>
2018-12-04 12:25:31 +0000

    Add files via upload

---
+++ mylib4/multiple_echo.v
@@ -0,0 +1,46 @@
+//------------------------------
+// Module name: allpass processor
+// Function: Simply to pass input to output
+// Creator:  Peter Cheung
+// Version:  1.1
+// Date:     24 Jan 2014
+//------------------------------
+
+module processor (sysclk, data_in, data_out, data_valid);
+
+	input				sysclk;		// system clock
+	input [9:0]		data_in;		// 10-bit input data
+	input 			data_valid;
+	output [9:0] 	data_out;	// 10-bit output data
+
+	wire				sysclk;
+	wire [9:0]		data_in;
+	reg [9:0] 		data_out;
+	wire [9:0]		x,y;
+	wire [9:0]		echo;
+	
+	wire 				full;
+	wire 				pulse;
+	wire				q;
+	
+	
+	parameter 		ADC_OFFSET = 10'h181;
+	parameter 		DAC_OFFSET = 10'h200;
+	
+	assign q = full & pulse;
+	assign x = data_in[9:0] - ADC_OFFSET;		// x is input in 2's complement
+	
+	// This part should include your own processing hardware 
+	// ... that takes x to produce y
+	pulse_gen GG(pulse, data_valid, sysclk);
+	fifo	FLY(sysclk, y,  q, pulse, full, echo);
+	
+
+	assign y = x - {echo[9], echo[9:1]};
+	
+	//  Now clock y output with system clock
+	always @(posedge sysclk)
+		data_out <=  y + DAC_OFFSET;
+		
+endmodule
+	
